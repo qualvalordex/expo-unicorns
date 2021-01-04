2d4cef4e5cea22b8d6f27aa4df9e6659fecfcb28
Paulo Rodrigues <qualvalordex@gmail.com>
2021-01-04 18:15:35 -0300

    Alteração no layout da tela de tradução

--- screens/CardScreen.js
+++ screens/CardScreen.js
@@ -9,68 +9,50 @@ import getCardImage from '../misc/getCardImage'
 
 export default function CardScreen({ navigation, route }) {
     return(
-        <SafeAreaView style={SharedStyles.container}>
-            <ImageBackground
-                source={require('../assets/bg_menu.png')}
-                style={SharedStyles.backgroundImage}
-            >
-                <View style={SharedStyles.backgroundMask}>
-                    <View style={[SharedStyles.content, { justifyContent:'center' }]}>
-                        <View style={styles.header}>
-                            <Text style={[SharedStyles.defaultTitle, { textAlign:'center', lineHeight:50, fontSize:50 }]}>
-                                { route.params.card_name }
-                            </Text>
-                        </View>
-                        <View style={styles.body}>
-                            <Image
-                                source={getCardImage(route.params.card_id)}
-                                style={styles.cardImage}
-                            />
-                            <View style={styles.translationContainer}>
-                                <Text style={SharedStyles.defaultSubtitle}>EFEITO</Text>
-                                <View style={styles.translation}>
-                                    <Text style={[SharedStyles.screenText, {lineHeight:30, textAlign:'justify'}]}>
-                                        { route.params.card_effect }
-                                    </Text>
-                                </View>
-                            </View>
-                            <CustomButton value='VOLTAR'
-                                onPress={ () => navigation.pop() }
-                            />
-                        </View>
+        <ImageBackground
+            source={require('../assets/bg_menu.png')}
+            style={SharedStyles.backgroundImage}
+        >
+            <View style={SharedStyles.container}>
+                
+                <View style={[SharedStyles.header, { flex: 1 }]}>
+                    <Text style={[SharedStyles.defaultTitle, { textAlign:'center', lineHeight:50, fontSize:50 }]}>
+                        { route.params.card_name }
+                    </Text>
+                </View>
+                
+                <View style={[SharedStyles.body, { flex: 4, justifyContent:null }]}>
+                    <Image
+                        source={getCardImage(route.params.card_id)}
+                        style={styles.cardImage}
+                    />
+                    <Text style={[SharedStyles.defaultSubtitle, { marginTop: 10 }]}>EFEITO</Text>
+                    <View style={styles.translation}>
+                        <Text style={[SharedStyles.screenText, {lineHeight:30, textAlign:'justify'}]}>
+                            { route.params.card_effect }
+                        </Text>
                     </View>
                 </View>
-            </ImageBackground>
-        </SafeAreaView>
+                
+                <View style={SharedStyles.footer}>
+                    <CustomButton value='VOLTAR'
+                        onPress={ () => navigation.pop() }
+                    />
+                </View>
+
+            </View>
+        </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
-    header:{
-        width:'100%',
-        alignItems:'center',
-        justifyContent:'center',
-        padding:10,
-        marginBottom:10
-    },
-    body: {
-        width:'100%',
-        alignItems:'center',
-        justifyContent:'space-around'
-    },
-    translationContainer:{
-        width:'100%',
-        alignItems:'center',
-        justifyContent:'center',
-        marginVertical:25
-    },
     cardImage:{
-        width:'100%',
+        height:'50%',
         resizeMode:'contain'
     },
     translation:{
-        width:300,
-        marginTop:15,
+        width:'90%',
+        marginTop:10,
         backgroundColor:'#ffffffaa',
         borderRadius:15,
         borderColor:'#a356a6',
