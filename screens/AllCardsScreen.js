90e663b2d1e478384a9a53b6ee1f2f7d25a1255f
Paulo Rodrigues <qualvalordex@gmail.com>
2021-01-04 17:49:33 -0300

    Alteração no layout da tela que mostra as cartas

--- screens/AllCardsScreen.js
+++ screens/AllCardsScreen.js
@@ -21,80 +21,67 @@ export default function AllCardsScreen({ navigation, route }) {
         data = filterByQuery(route.params.query)
     }
     return(
-        <SafeAreaView style={SharedStyles.container}>
-            <ImageBackground
-                source={require('../assets/bg_menu.png')}
-                style={SharedStyles.backgroundImage}
-            >
-                <View style={SharedStyles.backgroundMask}>
-                    <View style={SharedStyles.content}>
-                        <View style={styles.header}>
-                            <Text style={SharedStyles.defaultTitle}>
-                                {route.params?.title}
-                            </Text>
-                        </View>
-                        <View style={styles.body}>
-                            <View style={styles.cardlist}>
-                                <FlatList
-                                    data={data}
-                                    keyExtractor={item => item.card_id}
-                                    numColumns={2}
-                                    renderItem={({item}) => {
-                                        return (
-                                            <View style={styles.flatListItem}>
-                                                <TouchableOpacity
-                                                    style={{width:'100%'}}
-                                                    onPress={() => navigation.navigate('CardScreen', {card_id:item.card_id, card_name:item.card_name, card_effect:item.card_effect})}
-                                                >
-                                                    <Image
-                                                        style={styles.cardImage}
-                                                        source={getCardImage(item.card_id)}
-                                                    />
-                                                </TouchableOpacity>
-                                            </View>
-                                        );
-                                    }}
-                                />
-                            </View>
-                            <CustomButton value='VOLTAR'
-                                onPress={ () => navigation.pop() }
-                            />
-                        </View>
-                    </View>
+        <ImageBackground
+            source={require('../assets/bg_menu.png')}
+            style={SharedStyles.backgroundImage}
+        >
+            <View style={SharedStyles.container}>
+                
+                <View style={SharedStyles.header}>
+                    <Text style={SharedStyles.defaultTitle}>
+                        {route.params?.title}
+                    </Text>
                 </View>
-            </ImageBackground>
-        </SafeAreaView>
+                
+                <View style={[SharedStyles.body, { flexDirection: 'row' }]}>
+                    <FlatList
+                        data={data}
+                        keyExtractor={item => item.card_id}
+                        numColumns={2}
+                        renderItem={({item}) => {
+                            return (
+                                <View style={styles.flatListItem}>
+                                    <TouchableOpacity
+                                        style={{width:'100%'}}
+                                        onPress={() => navigation.navigate('CardScreen', {card_id:item.card_id, card_name:item.card_name, card_effect:item.card_effect})}
+                                    >
+                                        <Image
+                                            style={styles.cardImage}
+                                            source={getCardImage(item.card_id)}
+                                        />
+                                    </TouchableOpacity>
+                                </View>
+                            );
+                        }}
+                    />
+                </View>
+
+                <View style={SharedStyles.footer}>
+                    <CustomButton value='VOLTAR'
+                        onPress={ () => navigation.pop() }
+                    />
+                </View>
+            </View>
+        </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
-    header:{
-        flexDirection:'row',
-        width:'100%',
-        alignItems:'center',
-        justifyContent:'center',
-        padding:10
-    },
-    body: {
-        flexDirection:'column',
-        width:'100%',
-        alignItems:'center',
-        justifyContent:'space-around',
-    },
     cardlist: {
-        width:350,
-        height:600,
-        marginBottom:15,
+        width:'100%',
+        height:'100%',
         alignItems:'center'
     },
     flatListItem: {
-        width:160,
-        height:240,
+        flex: 1/2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height:250,
         margin:5
     },
     cardImage: {
         width:'100%',
         height:'100%',
-        resizeMode:'stretch'
+        resizeMode:'contain'
     }
 });
