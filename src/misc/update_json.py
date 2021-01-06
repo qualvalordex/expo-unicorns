63fec0e7af9067774d306ab50793f417fa08250e
Paulo Rodrigues <qualvalordex@gmail.com>
2021-01-06 18:48:49 -0300

    Adicionar cartas segunda edição

---
+++ src/misc/update_json.py
@@ -0,0 +1,15 @@
+import json
+
+with open('../../data/uucards.json') as json_file:
+    data = json.load(json_file)
+
+f = "export default function getCardImage(cardID) {\n\tswitch(cardID) {\n"
+
+for n, item in enumerate(data):
+    f += "\t\tcase " + str(n+1) + ":\n\t\t\treturn require('../../assets/images" + data[n]['card_path'] + "');\n"
+
+f += "\t\tdefault:\n\t\t\treturn require('../../assets/images/unicorn_not_found.png');\n\t}\n}"
+
+with open('getCardImage.js', 'w') as js_file:
+    js_file.write(f)
+    js_file.close()
